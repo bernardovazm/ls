3fc2490b90429409ffcfc6cffda1b7de49d6c962
Bernardo Vaz <bernardovazmelo@gmail.com>
2024-09-04 01:29:15 -0300

    Update relay

--- api/relayData.php
+++ api/relayData.php
@@ -1,10 +1,34 @@
 <?php
-phpinfo();
 header('Content-Type: application/json');
 
-// Lê os dados recebidos do frontend
-$input = json_decode(file_get_contents('php://input'), true);
+ob_start();
 
-// Simplesmente retransmite os dados recebidos
-echo json_encode($input);
-?>
+session_start();
+
+if (!isset($_SESSION['userData'])) {
+    $_SESSION['userData'] = [
+        'trackedUserIDs' => [], 
+        'userDescriptions' => [] 
+    ];
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $input = json_decode(file_get_contents('php://input'), true);
+
+    if (isset($input['trackedUserIDs'])) {
+        $_SESSION['userData']['trackedUserIDs'] = $input['trackedUserIDs'];
+    }
+    if (isset($input['userDescriptions'])) {
+        $_SESSION['userData']['userDescriptions'] = $input['userDescriptions'];
+    }
+    echo json_encode(['status' => 'success']);
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    ob_end_clean();
+    echo json_encode($_SESSION['userData']);
+    exit;
+}
+
+ob_end_clean();
