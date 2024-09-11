82aac61560b60f1f5cfcef4710f79a87f2902510
Bernardo Vaz <77335626+bernardovazm@users.noreply.github.com>
2024-09-11 02:01:46 -0300

    Update message display and implement date

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const peer = new Peer();
 let conn;
+let currentDate;
 
 peer.on("open", (id) => {
   document.getElementById("yourPeerId").innerText = `Your ID: ${id}`;
@@ -10,27 +11,37 @@ document.getElementById("sendButton").addEventListener("click", () => {
   const message = document.getElementById("messageInput").value;
   if (!conn) {
     conn = peer.connect(peerId);
-    setupConnection();
+    setupConnection(peerId);
   }
   if (message) {
     conn.send(message);
-    displayMessage("You: ", message);
+    displayMessage("You", message);
     document.getElementById("messageInput").value = "";
   }
 });
 
-function setupConnection() {
+function setupConnection(otherUser) {
   conn.on("open", () => {
     console.log("P2P connection established successfully.");
   });
   conn.on("data", (data) => {
-    displayMessage("Other user: ", data);
+    displayMessage(otherUser, data);
   });
 }
 
 function displayMessage(sender, message) {
   const receivedMessages = document.getElementById("receivedMessages");
-  receivedMessages.innerHTML += `<li><strong>${sender}:</strong> ${message}</li>`;
+  if (receivedMessages.innerHTML == "No messages :(") {
+    receivedMessages.innerHTML = "";
+  }
+  if (new Date().toLocaleDateString() !== currentDate) {
+    receivedMessages.innerHTML += `<p>${new Date().toLocaleDateString()}</p>`;
+  }
+  currentDate = new Date().toLocaleDateString();
+  receivedMessages.innerHTML += `<li><small>[${new Date().toLocaleTimeString(
+    "en-GB",
+    { hour: "numeric", minute: "numeric" }
+  )}]</small> <strong>${sender}:</strong> ${message}</li>`;
 }
 
 peer.on("connection", (incomingConn) => {
